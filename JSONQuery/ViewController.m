2dabdaa2f776e3f62766fca736dbf4cbd533e781
Chauster Kung <chausterkung@Grandex-Mac-mini.local>
2013-12-23 14:25:56 +0800

    JSONParse

--- JSONQuery/ViewController.m
+++ JSONQuery/ViewController.m
@@ -9,7 +9,6 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
 @end
 
 @implementation ViewController
@@ -18,6 +17,39 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"viewDidLoad");
+    [self wsLooking];
+}
+
+#define kBgQueue dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0) //1
+
+//抓取playList中的vidoe 遞迴取值
+- (void)wsLooking{
+    dispatch_async(kBgQueue, ^{
+
+        NSData *data = [NSData dataWithContentsOfURL:[NSURL URLWithString:@"url"]];
+        [self performSelectorOnMainThread:@selector(fetchedDataFromPlayList:) withObject:data waitUntilDone:YES];
+    });
+}
+
+-(void)fetchedDataFromPlayList:(NSData*)responseData {
+    
+    if (responseData) {
+        
+        NSError *error     = nil;
+        NSDictionary *json = [NSJSONSerialization
+                              JSONObjectWithData:responseData
+                              options:kNilOptions
+                              error:&error];
+        NSLog(@"%@",json);
+//        NSArray *catalogs_ = [[json objectForKey:@"feed"] objectForKey:@"entry"];
+//        for (int i = 0; i<[catalogs_ count]; i++) {
+//            NSDictionary *catalogItem = [catalogs_ objectAtIndex:i];
+//            NSString *name = [[(NSDictionary*)[[catalogItem objectForKey:@"author"] objectAtIndex:0] objectForKey:@"name"] objectForKey:@"$t"];
+//            NSLog(@"title = %@",name);
+//            NSLog(@"content = %@",[[(NSDictionary*)catalogItem objectForKey:@"content"]objectForKey:@"$t"]);
+//        }
+    }
 }
 
 - (void)didReceiveMemoryWarning
